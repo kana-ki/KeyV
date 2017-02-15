7bb1b0531bff4c7724c85949c0edd3720b79377f
Andy James <kana@ikimi.co>
2017-02-15 19:12:21 +0000

    Initial commit of KeyV

---
+++ KeyV/App.cs
@@ -0,0 +1,54 @@
+﻿using System;
+using System.Windows.Forms;
+
+namespace KeyV {
+
+    public class App {
+
+        readonly NotifyIcon _notifyIcon;
+        private readonly GlobalHotkey _globalHotkey;
+
+        public App() {
+            this._notifyIcon = this.CreateIcon();
+            this._globalHotkey = this.RegisterHotkey();
+            Application.Run();
+        }
+
+        private NotifyIcon CreateIcon() {
+            var notifyIcon = new NotifyIcon();
+            notifyIcon.Icon = Properties.Resources.Icon;
+            notifyIcon.Visible = true;
+            notifyIcon.ContextMenu = new ContextMenu(new[] {
+                new MenuItem("Exit KeyPaste", this.Exit_Click)
+            });
+            return this._notifyIcon;
+        }
+
+        private void Exit_Click(object obj, EventArgs args) {
+            Application.Exit();
+        }
+
+        private GlobalHotkey RegisterHotkey() {
+            var hotkey = new GlobalHotkey();
+            hotkey.RegisterGlobalHotKey((int)Keys.V, GlobalHotkey.MOD_CONTROL | GlobalHotkey.MOD_ALT);
+            hotkey.HotKeyPressed += this.HotKeyPressed;
+            return hotkey;
+        }
+
+        private void HotKeyPressed() {
+            if (Clipboard.ContainsText()) {
+                new Keyboard().PressKeys(Clipboard.GetText());
+            }
+        }
+
+        ~App() {
+            this._globalHotkey.Dispose();
+        }
+
+        [STAThread]
+        public static void Main() {
+            new App();
+        }
+
+    }
+}
---
+++ KeyV/Keyboard.cs
@@ -0,0 +1,53 @@
+﻿using System.Runtime.InteropServices;
+using System.Threading;
+using System.Windows.Forms;
+
+namespace KeyV {
+
+    public class Keyboard {
+
+        [DllImport("user32.dll", SetLastError = true)]
+        private static extern void keybd_event(byte bVk, byte bScan, int dwFlags, int dwExtraInfo);
+
+        [DllImport("user32.dll")]
+        static extern byte GetKeyState(byte bVk);
+
+        private const byte VK_SHIFT = 0x10;
+        private const byte VK_CONTROL = 0x11;
+        private const byte VK_ALT = 0x12;
+
+        private const byte KEY_PRESS = 0x0;
+        private const byte KEY_RELEASE = 0x2;
+
+        private bool _restoreShift = false;
+        private bool _restoreControl = false;
+        private bool _restoreAlt = false;
+
+        public void PressKeys (string str) {
+            this.ClearModifiers();
+            foreach (var ch in str) {
+                SendKeys.Send(ch.ToString());
+                Thread.Sleep(10);
+            }
+            this.RestoreModifiers();
+        }
+
+        private void ClearModifiers () {
+            this._restoreShift = (GetKeyState(VK_SHIFT) & 0x80) == 0x80;
+            this._restoreControl = (GetKeyState(VK_CONTROL) & 0x80) == 0x80;
+            this._restoreAlt = (GetKeyState(VK_ALT) & 0x8) == 0x80;
+            keybd_event(VK_SHIFT, 0, KEY_RELEASE, 0);
+            keybd_event(VK_ALT, 0, KEY_RELEASE, 0);
+            keybd_event(VK_CONTROL, 0, KEY_RELEASE, 0);
+        }
+
+        private void RestoreModifiers () {
+            if (this._restoreShift) keybd_event(VK_SHIFT, 0, KEY_PRESS, 0);
+            if (this._restoreControl) keybd_event(VK_CONTROL, 0, KEY_PRESS, 0);
+            if (this._restoreAlt) keybd_event(VK_ALT, 0, KEY_PRESS, 0);
+            this._restoreShift = this._restoreControl = this._restoreAlt = false;
+        }
+
+    }
+
+}
