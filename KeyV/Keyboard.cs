3d1eb3b014ec9c9b19b2f6a3691f78f09bf63b2e
Andy James <kana@ikimi.co>
2017-02-17 01:47:42 +0000

    Support chained hot keys without sticking

--- KeyV/Keyboard.cs
+++ KeyV/Keyboard.cs
@@ -1,4 +1,6 @@
-﻿using System.Collections.Generic;
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
 using System.Runtime.InteropServices;
 using System.Threading;
 using System.Windows.Forms;
@@ -7,19 +9,55 @@ namespace KeyV {
 
     public class Keyboard {
 
-        [DllImport("user32.dll", SetLastError = true)]
-        private static extern void keybd_event(byte bVk, byte bScan, int dwFlags, int dwExtraInfo);
-
-        private const byte VK_SHIFT = 0x10;
-        private const byte VK_CONTROL = 0x11;
-        private const byte VK_ALT = 0x12;
-
-        private const byte KEY_RELEASE = 0x2; // Press is 0x0
+        private const int WM_KEYUP = 0x101;
+        private const int WM_SYSKEYUP = 0x105;
+        private const byte WH_KEYBOARD_LL = 0x0D;
+        private const byte KEY_PRESS = 0x0;
+        private const byte KEY_RELEASE = 0x2;
 
         private static readonly Dictionary <char, string> _sanitization = new Dictionary <char, string> {
-            { '{', "{{}"}, { '}', "{}}"}, { '+', "{+}"}, { '^', "{^}"}, { '%', "{%}"},
-            { '~', "{~}"}, { '[', "{[}"}, { ']', "{]}"}, { '(', "{(}"}, { ')', "{)}"}
+            {'{', "{{}"},
+            {'}', "{}}"},
+            {'+', "{+}"},
+            {'^', "{^}"},
+            {'%', "{%}"},
+            {'~', "{~}"},
+            {'[', "{[}"},
+            {']', "{]}"},
+            {'(', "{(}"},
+            {')', "{)}"}
         };
+
+        private readonly Dictionary <Keys, bool> _userKeyMap = new Dictionary <Keys, bool> {
+            // Keys.Control, Keys.Alt, and Keys.Shift are modifiers - not keys
+            // Keys.ControlKeys, Keys.menu, Keys.ShiftKey are more like a property - true if either Left or Right counter part is true
+            // The following are the 'real' modifier *keys*
+            {Keys.LShiftKey, false},
+            {Keys.RShiftKey, false},
+            {Keys.LControlKey, false},
+            {Keys.RControlKey, false},
+            {Keys.LMenu, false},
+            {Keys.RMenu, false}
+        };
+
+        private IntPtr _userKeyHook;
+
+        [DllImport ("user32.dll", SetLastError = true)]
+        private static extern void keybd_event (byte bVk, byte bScan, int dwFlags, int dwExtraInfo);
+
+        [DllImport ("user32.dll")]
+        [return: MarshalAs (UnmanagedType.Bool)]
+        private static extern bool GetAsyncKeyState (byte bVk);
+
+        [DllImport ("user32.dll")]
+        private static extern IntPtr SetWindowsHookEx
+            (int idHook, KeyboardHookCallback callback, IntPtr hInstance, uint dwThreadId);
+
+        [DllImport ("user32.dll")]
+        private static extern bool UnhookWindowsHookEx (IntPtr hInstance);
+
+        [DllImport ("user32.dll", SetLastError = true)]
+        private static extern byte CallNextHookEx (IntPtr idHook, int nCode, int wParam, ref Keys key);
 
         public void AltTab () {
             this.ClearModifiers();
@@ -34,6 +72,7 @@ namespace KeyV {
                 SendKeys.SendWait(input);
                 Thread.Sleep(15);
             }
+            this.RestoreModifiers();
         }
 
         private string Sanitize (char ch) {
@@ -42,10 +81,29 @@ namespace KeyV {
         }
 
         private void ClearModifiers () {
-            keybd_event(VK_SHIFT, 0, KEY_RELEASE, 0);
-            keybd_event(VK_ALT, 0, KEY_RELEASE, 0);
-            keybd_event(VK_CONTROL, 0, KEY_RELEASE, 0);
+            foreach (var modifier in this._userKeyMap.Keys.ToList()) {
+                this._userKeyMap[modifier] = GetAsyncKeyState((byte) modifier);
+                keybd_event((byte) modifier, 0, KEY_RELEASE, 0);
+            }
+            this._userKeyHook = SetWindowsHookEx(WH_KEYBOARD_LL, this.OnModifierRelease, IntPtr.Zero, 0);
         }
+
+        private byte OnModifierRelease (int code, int wParam, ref Keys key) {
+            // Keys used by the sent keys will not be restored
+            if ((wParam == WM_KEYUP || wParam == WM_SYSKEYUP)
+                && this._userKeyMap.ContainsKey(key)) this._userKeyMap[key] = false;
+            return CallNextHookEx(this._userKeyHook, code, wParam, ref key);
+        }
+
+        private void RestoreModifiers () {
+            foreach (var modifier in this._userKeyMap.Keys.ToList()) {
+                if (this._userKeyMap[modifier]) keybd_event((byte) modifier, 0, KEY_PRESS, 0);
+                this._userKeyMap[modifier] = false;
+            }
+            UnhookWindowsHookEx(this._userKeyHook);
+        }
+
+        private delegate byte KeyboardHookCallback (int code, int wParam, ref Keys key);
 
     }
 
